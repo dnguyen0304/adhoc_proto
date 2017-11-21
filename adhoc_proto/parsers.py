3bb70ca24e4f53b62e408b03b1386e6eb9fd9b41
ubuntu <dnguyen0304@gmail.com>
2017-11-21 10:36:46 -0500

    Reverted "Removed HexToModel parser because it is not used."
    This reverts commit 2461aba1553393807859f2df19120b897fec4210.

--- adhoc_proto/parsers.py
+++ adhoc_proto/parsers.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import abc
+
 HEXADECIMAL = 16
+
+
+class HexToModel(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def parse(self, data):
+
+        """
+        Parse the data.
+
+        Parameters
+        ----------
+        data : typing.Iterable[str]
+            Sequence of bytes each encoded in base 16 (hexadecimal).
+
+        Returns
+        -------
+        adhoc_proto.models.Model
+        """
+
+        pass
 
 
 def convert_hex_to_int(data):
