553ec822b7f782c8d7195d4e534b0c2220b27814
ubuntu <dnguyen0304@gmail.com>
2017-11-21 12:55:18 -0500

    Added Record.

--- adhoc_proto/models.py
+++ adhoc_proto/models.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import suitcase.fields
 import suitcase.structure
+from suitcase.fields import (FieldProperty,
+                             Magic,
+                             UBInt8,
+                             UBInt32,
+                             UBInt64)
+
+from . import common
 
 PROTOCOL_NAME = 'MPS7'
 
 
 class Header(suitcase.structure.Structure):
 
-    protocol = suitcase.fields.Magic(expected_sequence=PROTOCOL_NAME)
-    version = suitcase.fields.UBInt8()
-    record_count = suitcase.fields.UBInt32()
+    protocol = Magic(expected_sequence=PROTOCOL_NAME)
+    version = UBInt8()
+    record_count = UBInt32()
+
+
+class Record(suitcase.structure.Structure):
+
+    _RECORD_TYPE_MAPPING = {
+        0x00: common.RecordType.DEBIT,
+        0x01: common.RecordType.CREDIT,
+        0x02: common.RecordType.START_AUTO_PAY,
+        0x03: common.RecordType.END_AUTO_PAY
+    }
+
+    _type = UBInt8()
+    timestamp = UBInt32()
+    users_id = UBInt64()
+
+    type = FieldProperty(field=_type,
+                         onget=lambda x: Record._RECORD_TYPE_MAPPING[x])
--- adhoc_proto/tests/test_models.py
+++ adhoc_proto/tests/test_models.py
@@ -2,7 +2,9 @@
 
 from nose.tools import assert_equals
 
+from .. import common
 from .. import models
+from .. import parsers
 
 
 def test_header_from_data():
@@ -13,8 +15,25 @@ def test_header_from_data():
     # expected.protocol is a magic string.
     expected.version = 1
     expected.record_count = 71
+
     header = models.Header.from_data(data=data)
 
     assert_equals(expected.protocol, header.protocol)
     assert_equals(expected.version, header.version)
     assert_equals(expected.record_count, header.record_count)
+
+
+def test_record_from_data():
+
+    #         \x00\x53\x09\x27\xD1\x39\x67\x47\xC0\x45\xD9\x91\x21
+    data = """\x00S\t'\xd19gG\xc0E\xd9\x91!"""
+    expected = models.Record()
+    expected.type = common.RecordType.DEBIT
+    expected.timestamp = parsers.convert_hex_to_int(data[1:5].encode('hex'))
+    expected.users_id = parsers.convert_hex_to_int(data[5:13].encode('hex'))
+
+    record = models.Record.from_data(data=data)
+
+    assert_equals(expected.type, record.type)
+    assert_equals(expected.timestamp, record.timestamp)
+    assert_equals(expected.users_id, record.users_id)
