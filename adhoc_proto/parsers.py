3d9219e4fa9a5555180b090fc83ed4fe9f0b4996
ubuntu <dnguyen0304@gmail.com>
2017-11-21 10:46:07 -0500

    Added abstractclassmethod.

--- adhoc_proto/parsers.py
+++ adhoc_proto/parsers.py
@@ -5,6 +5,19 @@ import abc
 HEXADECIMAL = 16
 
 
+# This implementation closely mirrors the one in the Python +3.2.
+# abc.abstractclassmethod was not added until Python 3.2. It was then
+# deprecated in Python 3.3 because being able to use classmethod and
+# abc.abstractmethod together was added.
+class abstractclassmethod(classmethod):
+
+    __isabstractmethod__ = True
+
+    def __init__(self, callable):
+        callable.__isabstractmethod__ = True
+        super(abstractclassmethod, self).__init__(callable)
+
+
 class HexToModel(object):
 
     __metaclass__ = abc.ABCMeta
