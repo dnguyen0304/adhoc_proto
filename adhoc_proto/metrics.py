afdc426923fe1ebc314d4ff64adefc4d976d1347
ubuntu <dnguyen0304@gmail.com>
2017-11-28 11:28:21 -0500

    Added calculate_debit_total.

---
+++ adhoc_proto/metrics.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import math
+
+from . import common
+
+
+def calculate_debit_total(log):
+
+    """
+    Calculate the total amount of debits.
+
+    The units are in dollars. The time complexity is O(n), where n
+    is the number of records in the log.
+
+    Parameters
+    ----------
+    log : typing.Sequence[adhoc_proto.models.Record]
+
+    Returns
+    -------
+    float
+    """
+
+    return math.fsum(record.amount
+                     for record
+                     in log
+                     if record.type == common.RecordType.DEBIT)
---
+++ adhoc_proto/tests/test_metrics.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from nose.tools import assert_equals
+
+from .. import common
+from .. import metrics
+from .. import models
+
+
+class TestCalculateDebitTotal(object):
+
+    def __init__(self):
+        self.log = None
+        self.expected = None
+
+    def setup(self):
+        self.log = list()
+        self.expected = 4.0
+
+        record_1 = models.Record()
+        record_2 = models.Record()
+        record_1.type = common.RecordType.DEBIT
+        record_2.type = common.RecordType.DEBIT
+        record_1.amount = 2.0
+        record_2.amount = 2.0
+
+        self.log.append(record_1)
+        self.log.append(record_2)
+
+    def test_sum(self):
+        debit_total = metrics.calculate_debit_total(log=self.log)
+        assert_equals(self.expected, debit_total)
+
+    def test_filter(self):
+        record_3 = models.Record()
+        record_3.type = common.RecordType.START_AUTO_PAY
+        record_3.amount = 2.0
+
+        self.log.append(record_3)
+
+        debit_total = metrics.calculate_debit_total(log=self.log)
+        assert_equals(self.expected, debit_total)
