dbda6fcd7e68878f04debf0ca30622fb6acf60f4
ubuntu <dnguyen0304@gmail.com>
2017-11-28 11:30:49 -0500

    Added calculate_credit_total.

--- adhoc_proto/metrics.py
+++ adhoc_proto/metrics.py
@@ -26,3 +26,26 @@ def calculate_debit_total(log):
                      for record
                      in log
                      if record.type == common.RecordType.DEBIT)
+
+
+def calculate_credit_total(log):
+
+    """
+    Calculate the total amount of credit.
+
+    The units are in dollars. The time complexity is O(n), where n
+    is the number of records in the log.
+
+    Parameters
+    ----------
+    log : typing.Sequence[adhoc_proto.models.Record]
+
+    Returns
+    -------
+    float
+    """
+
+    return math.fsum(record.amount
+                     for record
+                     in log
+                     if record.type == common.RecordType.CREDIT)
