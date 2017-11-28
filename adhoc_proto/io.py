468138cfd871f7f6c9f214ef09af341f84df64a8
ubuntu <dnguyen0304@gmail.com>
2017-11-27 23:07:23 -0500

    Added Record reader.

--- adhoc_proto/io.py
+++ adhoc_proto/io.py
@@ -2,6 +2,8 @@
 
 import abc
 
+import suitcase.exceptions
+
 from . import models
 
 
@@ -37,6 +39,32 @@ class Header(Reader):
         data = self._file.read(models.Header.LENGTH_BYTES)
         header = models.Header.from_data(data)
         return list(header)
+
+    def __repr__(self):
+        repr_ = '{}(file={})'
+        return repr_.format(self.__class__.__name__, self._file)
+
+
+class Record(Reader):
+
+    def __init__(self, file):
+
+        """
+        Parameters
+        ----------
+        file : file
+        """
+
+        self._file = file
+
+    def read(self):
+        data = self._file.read(models.Record.LENGTH_BYTES)
+        try:
+            record = models.Record.from_data(data)
+        except suitcase.exceptions.SuitcaseParseError:
+            record = models.Record.from_data(
+                data + self._file.read(models.Record.CONDITIONAL_LENGTH_BYTES))
+        return record
 
     def __repr__(self):
         repr_ = '{}(file={})'
