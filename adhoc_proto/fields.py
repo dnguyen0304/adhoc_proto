b29c9bb4f0d3d5b62b7b363613f3b0a639040e3c
ubuntu <dnguyen0304@gmail.com>
2017-11-27 20:52:29 -0500

    Added unpack override.

--- adhoc_proto/fields.py
+++ adhoc_proto/fields.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import struct
+
 from suitcase.fields import BaseStructField
 
 
@@ -15,3 +17,8 @@ class SBFloat32(BaseStructField):
 class SBFloat64(BaseStructField):
     """Signed Big Endian 64-bit float field."""
     PACK_FORMAT = UNPACK_FORMAT = b">d"
+
+    # As of November 27, 2017, the default implementation of unpack in
+    # suitcase.fields.BaseStructField does not support floats.
+    def unpack(self, data, **kwargs):
+        self._value = struct.unpack(self.UNPACK_FORMAT, data)[0]
